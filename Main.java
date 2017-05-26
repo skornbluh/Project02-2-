beda4843ed48a61bfb867b01939910a391e8d9a2
skornbluh <skornbluh@gmail.com>
2017-05-26 13:49:45 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,18 +2,19 @@ import java.io.File;
 import java.util.Scanner;
 
 public class Main {
-	private String[][] personsData;
+	private Person[] personData = null;
 	private String[][] assetsData;
 	private String[][] portfoliosData;
 	private int rowCount = 0;
+	private String[][] data = null;
 	private static final int PERSONS_COLUMN_COUNT = 5;
 	private static final int ASSETS_COLUMN_COUNT = 21;
 	private static final int PORTFOLIOS_COLUMN_COUNT = 10;
-	
+
 	private Scanner scanPersons = null;
 	private Scanner scanAssets = null;
 	private Scanner scanPortfolios = null;
-	
+
 	/**
 	 * constructor initializes all data
 	 */
@@ -28,21 +29,13 @@ public class Main {
 		}
 		for (int i = 0; i < 3; i++) {
 			Scanner scan = null;
-			String[][] data = null;
+
 			switch (i) {
 			case 0:
-				scan = scanPersons;
-				if (scan.hasNextInt()) {
-					rowCount = scan.nextInt();
-					scan.nextLine();
-				} else {
-					System.out.println("Bad file format, expected integer.");
-					System.exit(-1);
-				}
-				personsData = new String[rowCount][PERSONS_COLUMN_COUNT];
-				data = personsData;
+				makePerson(scanPersons);
 				break;
 			case 1:
+				// makeAssets(scanAssets)
 				scan = scanAssets;
 				if (scan.hasNextInt()) {
 					rowCount = scan.nextInt();
@@ -55,6 +48,7 @@ public class Main {
 				data = assetsData;
 				break;
 			case 2:
+				// makePortfolios(scanPortfolios)
 				scan = scanPortfolios;
 				if (scan.hasNextInt()) {
 					rowCount = scan.nextInt();
@@ -67,29 +61,109 @@ public class Main {
 				data = portfoliosData;
 				break;
 			}
-			
-			try {
-				getData(scan, data);
-			} catch (Exception e) {
-				e.printStackTrace();
-				System.exit(-1);
-			} finally {
-				scan.close();
+		}
+	}
+
+	/**
+	 * String firstName, String lastName, String street, String city, String
+	 * state, String zipCode, String country, String emailAddress, String secID
+	 * 
+	 * @param scanPersons
+	 */
+	private void makePerson(Scanner scanPersons) {
+		if (scanPersons.hasNextInt()) {
+			rowCount = scanPersons.nextInt();
+			personData = new Person[rowCount];
+			scanPersons.nextLine();
+		} else {
+			System.out.println("Bad file format, expected integer.");
+			System.exit(-1);
+		}
+
+		//
+		// create person object
+		//
+		String id = "";
+		String broker = "";
+		String firstName = "";
+		String lastName = "";
+		String street = "";
+		String city = "";
+		String state = "";
+		String zipCode = "";
+		String country = "";
+		String[] emailAddress = null;
+
+		for (int r = 0; r < personData.length; r++) {
+			String row = scanPersons.nextLine();
+			String[] column = row.split(";");
+			boolean isBroker = (!column[1].isEmpty());
+			String[] temp = null;
+
+			// create client object
+			for (int c = 0; c < column.length; c++) {
+				switch (c) {
+				case 0: // id
+					id = column[0];
+					break;
+				case 1: // broker
+					if (isBroker) {
+						broker = column[1];
+					}
+					break;
+				case 2: // full name
+					temp = column[2].split(",");
+					if (!temp[1].isEmpty()) {
+						firstName = temp[1];
+					}
+					if (!temp[0].isEmpty()) {
+						lastName = temp[0];
+					}
+					break;
+				case 3: // address
+					temp = column[3].split(",");
+					if (!temp[0].isEmpty()) {
+						street = temp[0];
+					}
+					if (!temp[1].isEmpty()) {
+						city = temp[1];
+					}
+					if (!temp[2].isEmpty()) {
+						state = temp[2];
+					}
+					if (!temp[3].isEmpty()) {
+						zipCode = temp[3];
+					}
+					if (!temp[4].isEmpty()) {
+						country = temp[4];
+					}
+					break;
+				case 4: // email addresses
+					temp = column[4].split(",");
+					emailAddress = new String[temp.length];
+					for (int f = 0; f < temp.length; f++) {
+						emailAddress[f] = temp[f];
+					}
+					break;
+				}
+			}
+			if (isBroker) {
+				personData[r] = new Broker(id, firstName, lastName, street, city, state, zipCode, country, emailAddress, broker);
+			} else {
+				personData[r] = new Client(id, firstName, lastName, street, city, state, zipCode, country, emailAddress);
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 		Main instance = new Main();
-		instance.printAllData(instance.getPersonsData());
 		instance.printAllData(instance.getAssetsData());
 		instance.printAllData(instance.getPortfoliosData());
-		
+
 	}
 
-
-	public String[][] getPersonsData() {
-		return personsData;
+	public Person[] getPersonsData() {
+		return personData;
 	}
 
 	public String[][] getAssetsData() {
@@ -98,17 +172,6 @@ public class Main {
 
 	public String[][] getPortfoliosData() {
 		return portfoliosData;
-	}
-
-	private void getData(Scanner scan, String[][] data) throws Exception {
-		
-		for (int r = 0; r < data.length; r++) {
-			String row = scan.nextLine();
-			String[] column = row.split(";");
-			for (int c = 0; c < column.length; c++) {
-				data[r][c] = column[c];
-			}
-		}
 	}
 
 	private void printAllData(String[][] data) {
