83d1369f13361e177a7fb8005e18fd928cc020a3
skornbluh <skornbluh@gmail.com>
2017-05-27 12:54:29 -0500

    Update and rename Stocks.java to StockAccount.java

---
+++ StockAccount.java
@@ -0,0 +1,68 @@
+public class StockAccount extends Account {
+	private double betaMeasure;
+	private String stockSymbol;
+	private double sharePrice;
+	private double quarterlyDividend;
+	private double baseRateOfReturn;
+	
+	public double getQuarterlyDividend() {
+		return quarterlyDividend;
+	}
+
+	public void setQuarterlyDividend(double quarterlyDividend) {
+		this.quarterlyDividend = quarterlyDividend;
+	}
+
+	public double getBaseRateOfReturn() {
+		return baseRateOfReturn;
+	}
+
+	public void setBaseRateOfReturn(double baseRateOfReturn) {
+		this.baseRateOfReturn = baseRateOfReturn;
+	}
+
+	public StockAccount(String code, String label, double quarterlyDividend, double baseRateOfReturn,
+			double betaMeasure, String stockSymbol, double sharePrice) {
+		super(code, label);
+			this.betaMeasure = betaMeasure;
+			this.stockSymbol = stockSymbol;
+			this.sharePrice = sharePrice;
+			
+		
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		System.out.println("*****************Stock Account***************** \n");
+		sb.append("Code: " + getCode() + " \n");
+		sb.append("Label: " + getLabel() + " \n");
+		sb.append("Quarterly Dividend: " + getQuarterlyDividend() + " \n");
+		sb.append("Base rate of return: " + getBaseRateOfReturn() + " \n");
+		sb.append("Beta Measure: " + getBetaMeasure() + " \n");
+		sb.append("Stock Symbol: " + getStockSymbol() + " \n");
+		sb.append("Share Price: " + getSharePrice() + " \n");
+
+		
+		
+		return sb.toString();
+	}
+	
+	public double getBetaMeasure() {
+		return betaMeasure;
+	}
+	public void setBetaMeasure(double betaMeasure) {
+		this.betaMeasure = betaMeasure;
+	}
+	public String getStockSymbol() {
+		return stockSymbol;
+	}
+	public double getSharePrice() {
+		return sharePrice;
+	}
+	public void setSharePrice(double sharePrice) {
+		this.sharePrice = sharePrice;
+	}
+	
+	
+
+}
