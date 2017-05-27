1bed9a1b75e77eda0db461c05a45a6a5fad96077
skornbluh <skornbluh@gmail.com>
2017-05-27 12:53:08 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,12 +3,9 @@ import java.util.Scanner;
 
 public class Main {
 	private Person[] personData = null;
-	private String[][] assetsData;
-	private String[][] portfoliosData;
+	private Account[] accountData = null;
+	private String[][] portfoliosData = null;
 	private int rowCount = 0;
-	private String[][] data = null;
-	private static final int PERSONS_COLUMN_COUNT = 5;
-	private static final int ASSETS_COLUMN_COUNT = 21;
 	private static final int PORTFOLIOS_COLUMN_COUNT = 10;
 
 	private Scanner scanPersons = null;
@@ -35,31 +32,21 @@ public class Main {
 				makePerson(scanPersons);
 				break;
 			case 1:
-				// makeAssets(scanAssets)
-				scan = scanAssets;
-				if (scan.hasNextInt()) {
-					rowCount = scan.nextInt();
-					scan.nextLine();
-				} else {
-					System.out.println("Bad file format, expected integer.");
-					System.exit(-1);
-				}
-				assetsData = new String[rowCount][ASSETS_COLUMN_COUNT];
-				data = assetsData;
+				makeAccount(scanAssets);
 				break;
-			case 2:
-				// makePortfolios(scanPortfolios)
-				scan = scanPortfolios;
-				if (scan.hasNextInt()) {
-					rowCount = scan.nextInt();
-					scan.nextLine();
-				} else {
-					System.out.println("Bad file format, expected integer.");
-					System.exit(-1);
-				}
-				portfoliosData = new String[rowCount][PORTFOLIOS_COLUMN_COUNT];
-				data = portfoliosData;
-				break;
+//			case 2:
+//				// makePortfolios(scanPortfolios)
+//				scan = scanPortfolios;
+//				if (scan.hasNextInt()) {
+//					rowCount = scan.nextInt();
+//					scan.nextLine();
+//				} else {
+//					System.out.println("Bad file format, expected integer.");
+//					System.exit(-1);
+//				}
+//				portfoliosData = new String[rowCount][PORTFOLIOS_COLUMN_COUNT];
+//				data = portfoliosData;
+//				break;
 			}
 		}
 	}
@@ -148,39 +135,109 @@ public class Main {
 				}
 			}
 			if (isBroker) {
-				personData[r] = new Broker(id, firstName, lastName, street, city, state, zipCode, country, emailAddress, broker);
+				personData[r] = new Broker(id, firstName, lastName, street, city, state, zipCode, country, emailAddress,
+						broker);
 			} else {
-				personData[r] = new Client(id, firstName, lastName, street, city, state, zipCode, country, emailAddress);
+				personData[r] = new Client(id, firstName, lastName, street, city, state, zipCode, country,
+						emailAddress);
+			}
+		}
+	}
+
+	/**
+	 * load the account objects
+	 * @param scanAssets
+	 */
+	private void makeAccount(Scanner scanAssets) {
+		if (scanAssets.hasNextInt()) {
+			rowCount = scanAssets.nextInt();
+			accountData = new Account[rowCount];
+			scanAssets.nextLine();
+		} else {
+			System.out.println("Bad file format, expected integer.");
+			System.exit(-1);
+		}
+		//
+		// create account objects
+		//
+		String code = "";
+		String label = "";
+		double apr = 0.0;
+		double quarterlyDividend = 0.0;
+		double baseRateOfReturn = 0.0;
+		double omegaMeasure = 0.0;
+		double totalValue = 0.0;
+		double betaMeasure = 0.0;
+		String stockSymbol = "";
+		double sharePrice = 0.0;
+
+		for (int r = 0; r < accountData.length; r++) {
+			String row = scanAssets.nextLine();
+			String[] column = row.split(";");
+			String accountType = column[1];
+
+			// create client object
+			for (int c = 0; c < column.length; c++) {
+				// all classes need this
+				code = column[0];
+				label = column[2];
+
+				switch (accountType) {
+				case "D": // store the fields and create DepositAccount
+					apr = Double.parseDouble(column[3]);
+					accountData[r] = new DepositAccount(code,label,apr);
+					break;
+				case "S": // store the fields and create StockAccount
+					quarterlyDividend = Double.parseDouble(column[3]);
+					baseRateOfReturn = Double.parseDouble(column[4]);
+					betaMeasure = Double.parseDouble(column[5]);
+					stockSymbol = column[6];
+					sharePrice = Double.parseDouble(column[7]);
+					accountData[r] = new StockAccount(code, label, quarterlyDividend, baseRateOfReturn,
+						 betaMeasure, stockSymbol, sharePrice);
+					break;
+				case "P": // store the fields and create PrivateInvestmentAccount
+					quarterlyDividend = Double.parseDouble(column[3]);
+					baseRateOfReturn = Double.parseDouble(column[4]);
+					omegaMeasure = Double.parseDouble(column[5]);
+					totalValue = Double.parseDouble(column[6]);
+					accountData[r] = new PrivateInvestmentAccount(code, label, quarterlyDividend, baseRateOfReturn, omegaMeasure, totalValue);
+					break;
+				}
 			}
 		}
 	}
 
 	public static void main(String[] args) throws Exception {
 		Main instance = new Main();
-		instance.printAllData(instance.getAssetsData());
-		instance.printAllData(instance.getPortfoliosData());
+		printPersons(instance.getPersonsData());
+		printAccounts(instance.getAccountData());
+	}
 
+	private static void printAccounts(Account[] accountData) {
+		for (int i = 0; i < accountData.length; i++) {
+			Account account = accountData[i]; // storing account types in account variable
+			System.out.println(account.toString());
+		}
+		
+	}
+
+	private static void printPersons(Person[] personsData) {
+		for (int i = 0; i < personsData.length; i++) {
+			Person person = personsData[i]; // 
+			System.out.println(person.toString());
+		}
 	}
 
 	public Person[] getPersonsData() {
 		return personData;
 	}
 
-	public String[][] getAssetsData() {
-		return assetsData;
+	public Account[] getAccountData() {
+		return accountData;
 	}
 
 	public String[][] getPortfoliosData() {
 		return portfoliosData;
-	}
-
-	private void printAllData(String[][] data) {
-		System.out.println("\n -- new file -- \n");
-		for (int r = 0; r < data.length; r++) {
-			for (int c = 0; c < data[0].length; c++) {
-				System.out.print(data[r][c] + "\t");
-			}
-			System.out.println();
-		}
 	}
 }
